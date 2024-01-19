28f847ab1daf5ac0c16377818ef3b1e183d65f33
houji <hj1254123@gmail.com>
2024-01-19 23:40:58 +0800

    chore: init

---
+++ .prettierrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  // 每行超过n列自动换行
+  printWidth: 100,
+  // 是否使用制表符，false表示用空格
+  useTabs: false,
+  // 缩进
+  tabWidth: 2,
+  // 结尾是否用分号
+  semi: true,
+  // 使用单引号
+  singleQuote: true,
+  // 在JSX中使用单引号
+  jsxSingleQuote: true,
+  // 箭头函数，如果只有一个参数，是否加括号。always加、avoid不加
+  arrowParens: 'always',
+  // Example: true - { foo: bar }、false - {foo: bar}
+  bracketSpacing: true,
+  // 尾随逗号 all|es5|none，默认 all
+  trailingComma: 'all',
+};
