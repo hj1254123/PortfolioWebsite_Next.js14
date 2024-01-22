340328b39658263da15d4c70bca81615ea60df12
houji <hj1254123@gmail.com>
2024-01-22 23:39:04 +0800

    feat: 完成 header 样式

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import './globals.css';
+import Header from '@/components/header';
 
 export const metadata: Metadata = {
   title: '猴几 | 个人作品集网站',
@@ -16,6 +17,7 @@ export default function RootLayout({
       <body className='relative bg-gray-50 text-gray-950'>
         <div className='absolute right-[11rem] top-[-6rem] -z-10 h-[31.25rem] w-[31.25rem] rounded-full bg-[#fbe2e3] blur-[10rem] sm:w-[68.75rem]'></div>
         <div className='absolute left-[-35rem] top-[-1rem] -z-10 h-[31.25rem] w-[50rem] rounded-full bg-[#dbd7fb] blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]'></div>
+        <Header />
         {children}
       </body>
     </html>
