598eb5e07698ac4692c6822fad38f0c15ee942d6
houji <hj1254123@gmail.com>
2024-01-23 23:34:15 +0800

    feat: 添加动画

--- components/about.tsx
+++ components/about.tsx
@@ -1,13 +1,20 @@
 'use client';
 
 import React from 'react';
+import { motion } from 'framer-motion';
 
 export default function About() {
   return (
     // scroll-mt-28 是 scroll-margin-top: 7rem/* 112px */;
     // 在本项目点击导航栏需要滚动到对应区域，默认会对齐视口顶部，
     // 但是会被 header 遮挡，用该属性可以给一个偏移距离。
-    <section id='about' className='mb-28 max-w-[45rem] scroll-mt-28 text-center leading-8 sm:mb-40'>
+    <motion.section
+      id='about'
+      className='mb-28 max-w-[45rem] scroll-mt-28 text-center leading-8 sm:mb-40'
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.175 }}
+    >
       <h2 className='mb-8 text-3xl font-medium'>About me</h2>
       <p className='mb-3'>
         {' '}
@@ -30,6 +37,6 @@ export default function About() {
         <span className='font-medium'>history and philosophy</span>. I&apos;m also learning how to
         play the guitar.
       </p>
-    </section>
+    </motion.section>
   );
 }
