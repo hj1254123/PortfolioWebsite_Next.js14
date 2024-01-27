fd14d6d64a6bc7d7adcb289bf55d0c012fa9c85f
houji <hj1254123@gmail.com>
2024-01-27 09:37:30 +0800

    feat: 平滑滚动；滚动到 home 设置上边距避免遮挡。

--- components/intro.tsx
+++ components/intro.tsx
@@ -12,7 +12,7 @@ import { FaGithubSquare } from 'react-icons/fa';
 
 export default function Intro() {
   return (
-    <section id='home' className='mb-28 max-w-[50rem] text-center sm:mb-0'>
+    <section id='home' className='mb-28 max-w-[50rem] scroll-mt-96 text-center sm:mb-0'>
       <div className='flex items-center justify-center'>
         <div className='relative'>
           <motion.div
